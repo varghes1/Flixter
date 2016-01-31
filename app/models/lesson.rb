7d8391d30cb8b3b4695a8221ea3d983c3a107de3
Bijoy Varghese <bijoy.varghese.1990@gmail.com>
2016-01-31 19:02:52 +0000

    Flixter lessons done

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,16 @@ class Lesson < ActiveRecord::Base
 	mount_uploader :video, VideoUploader
 	belongs_to :section
 
+	def next_lesson
+		lesson = section.lesson.where("row_order > ?", self.row_order).rank(:row_order).first
+		if lesson.blank? && section.next_section
+			return section.next_section.lessons.rank(:row_order).first
+		end
+		
+		return lesson
+
+	end
+
 	include RankedModel
 	ranks :row_order, :with_same => :section_id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
   resources :courses, :only => [:index, :show] do
     resources :enrollments, :only => [:create]
   end
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
     resources :sections, only: [:update]
     resources :lessons, only: [:update]
     resources :sections, only: [] do
-      resources :lessons, only: [:new, :create]
+      resources :lessons, only: [:create]
     end
     resources :courses, :only => [:new, :create, :show] do
       resources :sections, :only => [:create]
